ddeea79e74c4d9710521a4b39bc42e2db31afb9d
anurag8546 <2000520100019@ietlucknow.ac.in>
2023-10-15 21:35:13 +0530

    frontend added

---
+++ src/database/data.js
@@ -0,0 +1,60 @@
+ const st=[
+    {
+        id: 1,
+        question : "I spoke to ____.",
+        options : [
+            'she',
+            'her',
+            
+        ]
+    },
+    {
+        id: 2,
+        question : "Where ____ you come from?",
+        options : [
+            'do',
+            'is',
+            'am',
+            'are'
+        ]
+    },
+    {
+        id: 3,
+        question : "What time does she ___ up?",
+        options : [
+            'gets',
+            'get',
+        ]
+    },
+    {
+        id: 4,
+        question : " She ____ English twice a week",
+        options : [
+            'study',
+            'studies',
+            'studys',
+        ]
+    },
+    {
+        id: 5,
+        question : "There isn't ______ for all of us to get in.",
+        options : [
+            'space enough',
+            'no space enough',
+            'enough space',
+        ]
+    },
+    {
+        id: 6,
+        question : "How ___ you spell it?",
+        options : [
+            'is',
+            'am',
+            'are',
+            'do',
+        ]
+    }
+
+]
+export default st;
+export const answers = [1, 0, 1, 1, 2, 3];
